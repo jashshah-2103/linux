0df55858f96195aebeef06d1fbaa89b300f9c38f
jashshah-2103 <59500125+jashshah-2103@users.noreply.github.com>
2022-11-24 18:30:43 +0530

    Update bt3.sol

--- bt3.sol
+++ bt3.sol
@@ -1,50 +1,38 @@
-// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.11;
 
-pragma solidity >=0.7.0 <0.9.0;
-
-import "hardhat/console.sol";
-
-contract MarksManagmtSys {
-    // Structure
-    struct Student {
-        int256 ID;
-        string fName;
-        string lName;
-        int256 marks;
+contract StudentSystem
+{
+    struct Student{
+        uint roll_no;
+        string name;
+        string contact;
     }
 
-    address owner;
-    int256 public stdCount = 0;
-    //array
-    mapping(int256 => Student) public stdRecords;
+    Student[20] students;
+    uint i=0;
 
-    modifier onlyOwner() {
-        require(owner == msg.sender);
-        _;
+    function registerStudent(uint roll_no,string memory name,string memory contact) public returns(string memory)
+    {
+        if(i<20)
+        {
+            Student memory s=Student(roll_no,name,contact);
+            students[i]=s;
+            i=i+1;
+            return "Registered Successfully";
+        }
+        else return "Max Students Registered";
     }
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    // Create a function to add
-    // the new records
-    function addNewRecords(
-        int256 _ID,
-        string memory _fName,
-        string memory _lName,
-        int256 _marks
-    ) public onlyOwner {
-        // Increase the count by 1
-        stdCount = stdCount + 1;
-
-        // Fetch the student details
-        // with the help of stdCount
-        stdRecords[stdCount] = Student(_ID, _fName, _lName, _marks);
-    }
-
-    // fallback function
-    fallback() external {
-        console.log("Fallback function called");
+    function getDetails(uint roll_no) view public returns (string memory)
+    {
+        for(uint j=0;j<i;j++)
+        {
+            Student memory s=students[j];
+            if(s.roll_no==roll_no)
+            {
+                return string(bytes.concat("Name: ",bytes(s.name)," Contact: ",bytes(s.contact)));
+            }
+        }
+        return "Not Found";
     }
 }
